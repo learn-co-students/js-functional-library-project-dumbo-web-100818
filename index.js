85ade2cfd5ede051f4d81ce950ad074bebf63a19
hmalik88 <hbmalik88@gmail.com>
2018-11-24 23:13:39 -0500

    Done.

--- index.js
+++ index.js
@@ -4,20 +4,39 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(testArr, alert) {
+      const collection = testArr instanceof Array ? testArr.slice() : Object.values(testArr);
+      for (let i = 0; i < collection.length; i++) {
+        alert(collection[i]);
+      }
+      return testArr;
     },
 
-    map: function() {
-
+    map: function(testArr, callback) {
+      const collection = testArr instanceof Array ? testArr.slice() : Object.values(testArr);
+      const newArray = []
+      for (let i = 0; i < collection.length; i++) {
+        newArray.push(callback(collection[i]));
+      }
+      return newArray;
     },
 
-    reduce: function() {
-
+    reduce: function(testArr, callback, acc = 0) {
+      const collection = testArr instanceof Array ? testArr.slice() : Object.values(testArr);
+      for (let i = 0; i < collection.length; i++) {
+        acc = callback(acc, collection[i], collection);
+      }
+      return acc;
     },
-    
-    functions: function() {
 
+    functions: function(obj) {
+      const functionNames = [];
+      for (let key in obj) {
+        if (typeof obj[key] === "function") {
+          functionNames.push(key);
+        }
+      }
+      return functionNames.sort();
     },
 
 
